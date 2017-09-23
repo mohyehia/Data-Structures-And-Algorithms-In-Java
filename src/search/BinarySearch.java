3bc4100d1bfd4e1cb9dfb4f332fce386984a7eae
mohyehia <mohammedyehia99@gmail.com>
2017-09-23 21:16:37 +0200

    Implement BinarySearch

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -1,31 +1,36 @@
 package search;
-
 //Best Case => O(1)
 //Worst Case => O(log(n))
-
 public class BinarySearch {
 	public static void main(String[] args) {
 		int[] a = {3, 5, 8, 10, 12, 14, 15, 16, 23, 26};
-		//System.out.println(iterativeBinarySearch(a, 8));
-		System.out.println(recursiveBinarySearch(a, 0, a.length - 1, 5));
+		//System.out.println(iterativeBinarySearch(a, 16));
+		//System.out.println(recursiveBinarySearch(a, 27));
 	}
-	static int iterativeBinarySearch(int[] a, int num){
+	//iterative binary search
+	static int iterativeBinarySearch(int[] a, int target) {
 		int left = 0, right = a.length - 1, mid = 0;
-		while(left <= right){
+		while(left <= right) {
 			mid = (left + right) / 2;
-			if(num == a[mid]) return mid;
-			else if(num < a[mid]) right = mid - 1;
+			if(target == a[mid]) return mid;
+			else if(target < a[mid]) right = mid - 1;
 			else left = mid + 1;
 		}
 		return -1;
 	}
-	static int recursiveBinarySearch(int[] a, int left, int right, int num){
+	//recursive binary search
+	static int recursiveBinarySearch(int[] a, int target) {
+		return recursiveBinarySearch(a, 0, a.length - 1, target);
+	}
+	static int recursiveBinarySearch(int[] a, int left, int right, int target) {
 		int mid = 0;
-		if(left <= right){
+		if(left <= right) {
 			mid = (left + right) / 2;
-			if(num == a[mid]) return mid;
-			else if(num < a[mid]) return recursiveBinarySearch(a, left, mid - 1, num);
-			else return recursiveBinarySearch(a, mid + 1, right, num);
+			if(target == a[mid]) return mid;
+			else if(target < a[mid])
+				return recursiveBinarySearch(a, left, mid - 1, target);
+			else
+				return recursiveBinarySearch(a, mid + 1, right, target);
 		}
 		return -1;
 	}
